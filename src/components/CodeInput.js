31ded54eaed268c3181fdadcd8d05870c76f3b4e
nehpethP <sarkis8@uwindsor.ca>
2021-05-06 21:46:09 -0400

    form error on codeInput.js implemented
    #34

--- src/components/CodeInput.js
+++ src/components/CodeInput.js
@@ -8,48 +8,60 @@ import Card from './Card';
 const CodeInput = ({onCodeSubmit}) => {
    const [joinCode, setJoinCode] = useState('');
 
-   //const [error, setError] = useState(false);
+   const [error, setError] = useState(false);
 
    const onFormSubmit = (event) => {
       event.preventDefault();
       console.log('joinCode: ', joinCode);
-      onCodeSubmit(joinCode);
+
+
+      if(joinCode.length < 6 || joinCode > 0){
+         setError(true)
+      }else{
+        setError(false)
+      }
+
+      if(!error){
+        onCodeSubmit(joinCode);
+      }
+
    }
 
-
-   // const handleChange = (event) => {
-   //   event.preventDefault();
-   //
-   //   setJoinCode(event.target.value)
-   // }
 
    return (
      <Card width="20rem" padding="5rem 0 0 0">
         <div className="content">
-
-         <form className="ui large form"onSubmit={e => onFormSubmit(e)}>
+          <form className="ui large form"onSubmit={e => onFormSubmit(e)}>
             <div className="field" style={{marginBottom: "0.5rem"}}>
-
-               <input
-                  style={{fontSize: "2em", padding: "0", textAlign: "center"}}
-                  maxLength="6"
-                  type="text"
-                  placeholder="Code"
-                  value={joinCode}
-                  onChange={(e) => setJoinCode(e.target.value)}
-               />
+              <input
+                style={{fontSize: "2em", padding: "0", textAlign: "center"}}
+                maxLength="6"
+                type="text"
+                placeholder="Code"
+                value={joinCode}
+                onChange={e => setJoinCode(e.target.value)}
+              />
             </div>
-              <Button
-                 className="custom-button dark span"
-                 type="submit"
-              >
+            <Button
+               className="custom-button dark span"
+               type="submit"
+            >
               Join
-              </Button>
-            </form>
-            {/*error ? 'Code must be length 6' : ''*/}
-          </div>
-          </Card>
-
+            </Button>
+          </form>
+            {error
+              ?
+              <div
+                className="ui error message"
+                style={{textAlign: "center", padding: "0.25rem 0.25rem", marginTop: "0.5rem"}}
+              >
+                Invalid Meeting Code
+              </div>
+             :
+             ' '
+           }
+        </div>
+      </Card>
    );
 
 };
